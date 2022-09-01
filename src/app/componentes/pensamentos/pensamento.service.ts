6673ef4dc68eeb2b0bb6c6c84a9c0a5e35b81600
nayannelbatista <nayannelopes.bd@gmail.com>
2022-09-01 10:18:06 -0300

    Aula 5 finalizada

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -34,7 +34,11 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento )
+  }
 
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito
+    return this.editar(pensamento)
   }
 
   excluir(id: number): Observable<Pensamento> {
